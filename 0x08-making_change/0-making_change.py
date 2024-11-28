7090cc682ace37741fe598b8e4c154c6889f1ce2
KyakuwaPeninnah <pennykyakuwa@gmail.com>
2024-11-28 19:38:38 +0000

    Making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Task 1
+"""
+
+
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a total
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_counti
